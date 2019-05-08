b3f06ccf831a8aa708ffa32fa027280331e92ce7
Alexandra <fossum.alexandra@gmail.com>
2019-05-08 15:03:39 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ sayHello('World');
 /**
  * require style imports
  */
+const $ = require('jquery');
 const {getMovies} = require('./api.js');
+
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
@@ -18,3 +20,35 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+$("#add-movie-btn").click("addMovies");
+// document.getElementById("add-movie-btn").addEventListener("click", addMovies, false);
+
+function addMovies () {
+
+  console.log("It works!");
+  let title = document.getElementById("title").value;
+  let ratings = document.getElementById("ratings").value;
+
+  console.log(title);
+  console.log(ratings);
+
+  const moviePost = {title: `${title}`, rating: `${ratings}`, id: ""};
+  const url = '/api/movies';
+  const options = {
+    method: "POST",
+    body: JSON.stringify(moviePost),
+  }
+}
+
+// fetch(url, options)
+//     .then(console.log("success"))
+//     .catch(console.log("failure"));
+//
+// }
+
+
+
+
+
+
