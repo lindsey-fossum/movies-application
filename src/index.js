cb2c09f53f7fc52f6560aaa63691660ddaa6e042
Jason Lindsey <jasonlindsey@Jasons-MacBook-Pro.local>
2019-05-08 15:56:33 -0500

    dont be a menace

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
+const $ = require('jquery');
 
 /**
  * require style imports
  */
-const $ = require('jquery');
 const {getMovies} = require('./api.js');
 
+import sayHello from './hello';
+sayHello('World');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
@@ -21,33 +21,70 @@ getMovies().then((movies) => {
   console.log(error);
 });
 
-$("#add-movie-btn").click("addMovies");
+$('#edit-movie-btn').click(editMovie);
+$('#form-select').click(updateForm);
+$("#add-movie-btn").click(addMovies);
 // document.getElementById("add-movie-btn").addEventListener("click", addMovies, false);
 
 function addMovies () {
-
   console.log("It works!");
-  let title = document.getElementById("title").value;
-  let ratings = document.getElementById("ratings").value;
+  let title = $('#title').val();
+  let ratings = $('#ratings').val();
 
-  console.log(title);
-  console.log(ratings);
+  alert(title);
+  alert(ratings);
 
   const moviePost = {title: `${title}`, rating: `${ratings}`, id: ""};
   const url = '/api/movies';
   const options = {
     method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(moviePost),
+  }
+fetch(url, options)
+    .then(console.log(url), console.log(options))
+    .catch(console.log("failure"));
+}
+
+function updateForm(){
+  let form = $('#hoobajooba').val()
+  if (form === 'edit'){
+    makeEditForm()
   }
 }
 
-// fetch(url, options)
-//     .then(console.log("success"))
-//     .catch(console.log("failure"));
-//
-// }
+function makeEditForm(){
+  let html = "<label for=\"movie-to-edit\">Choose a Movie</label>\n" +
+      "        <select name=\"movies\" id=\"movie-to-edit\">\n" +
+      "            \n" +
+      "        </select>\n" +
+      "        <label for=\"new-movie-ratings\">Enter Movie Rating: </label>\n" +
+      "            <select id=\"new-movie-ratings\">\n" +
+      "                <option value=\"1\">1</option>\n" +
+      "                <option value=\"2\">2</option>\n" +
+      "                <option value=\"3\">3</option>\n" +
+      "                <option value=\"4\">4</option>\n" +
+      "                <option value=\"5\">5</option>\n" +
+      "            </select>\n" +
+      "        <button id=\"edit-movie-btn\">Edit Movie</button>"
 
+  $('#change-form').html(html);
 
+  getMovies().then((movies)=>{
+    let options;
+    movies.forEach((element)=>{
+      options = `<option>${element.title}</option>`;
+          $('#movie-to-edit').append(options)
+    })
+  });
+
+}
+
+function editMovie(){
+
+}
 
 
 
